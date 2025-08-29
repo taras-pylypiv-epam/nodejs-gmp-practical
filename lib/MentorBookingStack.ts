03b440f078f00df7aebd7a090efb40b735c5471e
taras-pylypiv-epam <75982205+taras-pylypiv-epam@users.noreply.github.com>
2025-08-29 17:27:45 +0300

    Initial configuration (#1)
    * Install prettier
    
    * Update ignore lists
    
    * Install eslint
    
    * Install husky
    
    * Fix indentation
    
    * Add CDK workflow

--- lib/MentorBookingStack.ts
+++ lib/MentorBookingStack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 export class MentorBookingStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
-  }
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+    }
 }
